768840c7a028c0e244b27466a25b04f83e3e39aa
obukhov <aov@amboss.com>
2019-02-05 00:19:40 +0100

    Add concurrent restorer

--- cmd/copy.go
+++ cmd/copy.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/mediocregopher/radix/v3"
 	"log"
+	"sync"
 	"time"
 
 	"github.com/spf13/cobra"
@@ -12,18 +13,22 @@ import (
 var pattern string
 var scanCount, report, limit int
 
+type keyDump struct {
+	key   string
+	value string
+	ttl   int
+}
+
 var copyCmd = &cobra.Command{
 	Use:   "copy [sourceHost:port] [targetHost:port]",
 	Short: "Copy keys from source redis instance to destination by given pattern",
 	Long:  ``,
 	Args:  cobra.MinimumNArgs(2),
 	Run: func(cmd *cobra.Command, args []string) {
-		start := time.Now()
 
 		fmt.Println("Start copying")
 
 		clientSource, err := radix.DefaultClientFunc("tcp", args[0])
-
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -33,65 +38,102 @@ var copyCmd = &cobra.Command{
 			log.Fatal(err)
 		}
 
-		scanOpts := radix.ScanOpts{
-			Command: "SCAN",
-			Count:   scanCount,
+		scanner := createScanner(clientSource)
+
+		keyChannel := make(chan keyDump)
+
+		waitingGroup := new(sync.WaitGroup)
+
+		for i := 0; i < 10; i++ {
+			waitingGroup.Add(1)
+			go restorer(keyChannel, clientTarget, waitingGroup)
 		}
 
-		if pattern != "*" {
-			scanOpts.Pattern = pattern
-		}
+		readKeys(clientSource, keyChannel, scanner)
+		close(keyChannel)
 
-		scanner := radix.NewScanner(clientSource, scanOpts)
-
-		var key string
-		counter := 0
-		cycle := 0
-		cycleStart := time.Now()
-		for scanner.Next(&key) {
-
-			var value string
-			var ttl int
-
-			p := radix.Pipeline(
-				radix.Cmd(&ttl, "PTTL", key),
-				radix.Cmd(&value, "DUMP", key),
-			)
-
-			if err := clientSource.Do(p); err != nil {
-				panic(err)
-			}
-
-			if ttl < 0 {
-				ttl = 0
-			}
-
-			err = clientTarget.Do(radix.FlatCmd(nil, "RESTORE", key, ttl, value, "REPLACE"))
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			counter++
-			cycle++
-
-			if cycle == report {
-				log.Printf("Copied another %d keys in: %s", report, time.Since(cycleStart))
-				cycle = 0
-				cycleStart = time.Now()
-			}
-
-			if limit > 0 && counter > limit {
-				fmt.Printf("Early exit after %d keys copied\n", counter)
-				return
-			}
-		}
+		waitingGroup.Wait()
 
 		if err := scanner.Close(); err != nil {
 			log.Fatal(err)
 		}
 
-		log.Printf("In total %d keys copied in %s", counter, time.Since(start))
 	},
+}
+
+func createScanner(clientSource radix.Client) radix.Scanner {
+	scanOpts := radix.ScanOpts{
+		Command: "SCAN",
+		Count:   scanCount,
+	}
+
+	if pattern != "*" {
+		scanOpts.Pattern = pattern
+	}
+
+	return radix.NewScanner(clientSource, scanOpts)
+}
+
+func restorer(keyChannel chan keyDump, clientTarget radix.Client, waitingGroup *sync.WaitGroup) {
+	for {
+		key, more := <-keyChannel
+
+		if more {
+			err := clientTarget.Do(radix.FlatCmd(nil, "RESTORE", key.key, key.ttl, key.value, "REPLACE"))
+			if err != nil {
+				log.Fatal(err)
+			}
+		} else {
+			break
+		}
+	}
+
+	waitingGroup.Done()
+}
+
+func readKeys(clientSource radix.Client, keyChannel chan keyDump, scanner radix.Scanner) {
+	var key string
+	counter := 0
+	cycle := 0
+
+	cycleStart := time.Now()
+	start := time.Now()
+
+	for scanner.Next(&key) {
+
+		var value string
+		var ttl int
+
+		p := radix.Pipeline(
+			radix.Cmd(&ttl, "PTTL", key),
+			radix.Cmd(&value, "DUMP", key),
+		)
+
+		if err := clientSource.Do(p); err != nil {
+			panic(err)
+		}
+
+		if ttl < 0 {
+			ttl = 0
+		}
+
+		keyChannel <- keyDump{key: key, value: value, ttl: ttl}
+		counter++
+		cycle++
+
+		if cycle == report {
+			log.Printf("Copied another %d keys in: %s", report, time.Since(cycleStart))
+			cycle = 0
+			cycleStart = time.Now()
+		}
+
+		if limit > 0 && counter > limit {
+			fmt.Printf("Early exit after %d keys copied\n", counter)
+			return
+		}
+	}
+
+	log.Printf("In total %d keys copied in %s", counter, time.Since(start))
 }
 
 func init() {
